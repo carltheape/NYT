2fdffbbdac3724a171d6573283cab21c087e6fe9
Ryan Hardeman <Ryan Hardeman>
2017-06-03 12:59:28 -0400

    update to js

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+var search = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+var apiKey = "8c827b429dc2450280570c08e85e192f";
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey;
+
+
+//grab information from search fields
+
+//insert search term into api url
+
+//ajax call to api
+
+$.ajax({
+	url: queryUrl,
+	method: "GET"
+}).done(function(response){
+	console.log(response);
+});
