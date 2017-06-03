3f9dc9e524b901049ed0f0a0c2b16339415dd10b
Ryan Hardeman <Ryan Hardeman>
2017-06-03 13:30:06 -0400

    js links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-var search = "";
+var search = "star";
 var numResults = 0;
 var startYear = 0;
 var endYear = 0;
 var articleCounter = 0;
 
 var apiKey = "8c827b429dc2450280570c08e85e192f";
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=star";
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + search;
 
 
 //grab information from search fields
@@ -14,13 +14,19 @@ var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key
 
 //ajax call to api
 
-$.ajax({
-	url: queryUrl,
-	method: "GET"
-}).done(function(response){
-	console.log(response);
-});
+function runQuery (queryUrl){
 
-$(".search").on("click"), function(){
-	alert("I'm Searching")
+	$.ajax({
+		url: queryUrl,
+		method: "GET"
+	}).done(function(response){
+		console.log(response);
+	});
+
+	$(".search").on("click"), function(){
+		alert("I'm Searching")
+	}
+
+
 }
+
