e9ba4b2f4929a5fe5fc4ae1b551a39f08fef2e04
Ryan Hardeman <Ryan Hardeman>
2017-06-03 13:14:33 -0400

    added js links to html

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,10 @@ var search = "";
 var numResults = 0;
 var startYear = 0;
 var endYear = 0;
+var articleCounter = 0;
 
 var apiKey = "8c827b429dc2450280570c08e85e192f";
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey;
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=star";
 
 
 //grab information from search fields
